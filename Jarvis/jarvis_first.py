f8669bb56b92a2d29693b0811e1e1b8b0a15276d
NarekHarutyu <120764847+NarekHarutyu@users.noreply.github.com>
2023-06-26 14:11:39 -0400

    Add files via upload

---
+++ Jarvis/jarvis_first.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+import subprocess
+import sys
+
+def submit_text():
+    text = entry.get()
+    # print("You entered:", text)
+    
+    if text == "a":
+        text = entry.get()
+        print("You entered:", text)
+        subprocess.run([sys.executable, "hello_world.py"])
+        subprocess.run([sys.executable, "hello_world1.py"])
+        
+    root.destroy()
+
+root = tk.Tk()
+root.title("Text Input")
+
+# Create input field
+entry = tk.Entry(root)
+entry.pack()
+
+# Create submit button
+submit_button = tk.Button(root, text="Submit", command=submit_text)
+submit_button.pack()
+
+root.mainloop()
+
+
+
+# import tkinter as tk
+# import subprocess
+# import sys
+
+# def submit_text():
+#     text = entry.get()
+#     # print("You entered:", text)
+    
+#     if text == "a":
+#         text = entry.get()
+#         print("You entered:", text)
+        
+#         subprocess.run([sys.executable, "hello_world.py"])
+#         subprocess.run([sys.executable, "hello_world1.py"])
+
+#     root.destroy()
+
+# root = tk.Tk()
+# root.title("Text Input")
+
+# # Create input field
+# entry = tk.Entry(root)
+# entry.pack()
+
+# # Create submit button
+# submit_button = tk.Button(root, text="Submit", command=submit_text)
+# submit_button.pack()
+
+# root.mainloop()
+
+
